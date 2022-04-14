d565c5049a4751142f2c36198206234615665b3c
dcr28 <dcr28@alu.ua.es>
2022-04-14 20:17:46 +0200

    49762328B Metodos ENComentarios

--- library/ENComentarios.cs
+++ library/ENComentarios.cs
@@ -8,8 +8,31 @@ namespace library
 {
     public class ENComentarios
     {
-       
+        private int _Id;
+        private string _Texto;
+        private int _Estrellas;
+        private string _Nickname;
 
-
+        public int Id //GET-SET
+        {
+            get { return _Id; }
+            set { _Id = value; }
+        }
+        public string Texto //GET-SET
+        {
+            get { return _Texto; }
+            set { _Texto = value; }
+        }
+        public int Estrellas //GET-SET
+        {
+            get { return _Estrellas; }
+            set { _Estrellas = value; } 
+        }
+        public string Nickname //GET-SET
+        {
+            get { return _Nickname; }
+            set { _Nickname = value; }
+        }
+        
     }
 }
