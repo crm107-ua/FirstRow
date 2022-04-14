18a8e4afc8adef5e159781f50ca197d1d30c3245
jmcl9 <jmcl9@alu.ua.es>
2022-04-14 13:04:16 +0200

    Implementacion CADCategoria.cs

--- library/ENUsuario.cs
+++ library/ENUsuario.cs
@@ -8,35 +8,6 @@ namespace library
 {
     public class ENUsuario
     {
-        internal int idViaje;
-        internal int nombreUsuario;
-        internal int TourViaje;
-    
-
-        public int id
-        {
-            get { return idViaje; }
-            private set { idViaje = value; }
-        }
-
-        public int Ciudad_pais
-        {
-            get { return Ciudad_pais; }
-            private set { idViaje = value; }
-        }
-
-        public int Tour_Viaje
-        {
-            get { return Tour_Viaje; }
-            private set { idViaje = value; }
-        }
-
-        public static string Titulo; //CRUD
-
-        public static string desempeño; //CRUD
-
-        public static string imagenes; //Add-Delete
-
         
     }
 }
