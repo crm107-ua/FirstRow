ddaf7f1284a855cee9623f9f7b5c62360727506a
Carlos Robles <crm107@alu.ua.es>
2022-04-28 22:20:16 +0200

    Correccion de menus 2

--- FirstRow/Pages/Story.aspx.designer.cs
+++ FirstRow/Pages/Story.aspx.designer.cs
@@ -15,6 +15,51 @@ namespace FirstRow.Pages
     {
 
         /// <summary>
+        /// Control login_sect.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl login_sect;
+
+        /// <summary>
+        /// Control register_sect.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl register_sect;
+
+        /// <summary>
+        /// Control settings_sect.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl settings_sect;
+
+        /// <summary>
+        /// Control settings_emp_sect.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl settings_emp_sect;
+
+        /// <summary>
+        /// Control logout_sect.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl logout_sect;
+
+        /// <summary>
         /// Control stories_items.
         /// </summary>
         /// <remarks>
