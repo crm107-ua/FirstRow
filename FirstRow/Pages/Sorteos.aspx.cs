500beed1435d6f2d5f57a50d22e34caf660ea792
Franco Ezequiel Gonzalez Quintana <fegq1@alu.ua.es>
2022-04-29 11:53:38 +0200

    48799706P manejador  crear sorteos

--- FirstRow/Pages/Sorteos.aspx.cs
+++ FirstRow/Pages/Sorteos.aspx.cs
@@ -4,14 +4,26 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using library;
 
 namespace FirstRow.Pages
 {
     public partial class Sorteos : System.Web.UI.Page
     {
+        ENSorteos sorteo;
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
+        public event EventHandler<CrearSorteoArgs> crearsorteo;
     }
+    public class CrearSorteoArgs : EventArgs
+    {
+        public ENSorteos sorteo { get; set; }
+        public CrearSorteoArgs (ENSorteos sorteo)
+        {
+            this.sorteo = sorteo;
+        }
+    }
+
 }
