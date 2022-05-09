0e6e1ec5196d477c34e7c200f55202c57989c1a6
jcm179-ua <jcm179@alu.ua.es>
2022-05-09 21:21:40 +0200

    47431594M Formurario de galeria empezado falta la validacion de los datos

---
+++ FirstRow/Pages/Forms/FormGaleria.aspx.cs
@@ -0,0 +1,50 @@
+﻿using library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace FirstRow.Pages.Forms
+{
+    public partial class FormGaleria : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["User"] != null)
+            {
+                ENPais pais = new ENPais();
+                List<ENPais> paises = new List<ENPais>();
+                pais.readPaises(paises);
+
+                foreach (ENPais p in paises)
+                {
+                    listaPaises_form_experiencia.Items.Insert(0, new ListItem(p.name, p.id.ToString()));
+                }
+            }
+            else
+            {
+                Page.ClientScript.RegisterClientScriptBlock(GetType(), "register_user_rollback", "setTimeout(ClickTheLink,500); function ClickTheLink() { document.getElementById('register_user_pop_up').click(); }", true);
+            }
+
+        }
+
+        protected void btnCrea_Click(object sender, EventArgs e)
+        {
+            HttpFileCollection _HttpFileCollection = Request.Files;
+            
+            if (_HttpFileCollection.Count > 6) 
+            {
+            }
+            //Primero validar los datos
+            
+            for (int i = 0; i < _HttpFileCollection.Count; i++)
+            {
+                HttpPostedFile _HttpPostedFile = _HttpFileCollection[i];
+                if (_HttpPostedFile.ContentLength > 0) _HttpPostedFile.SaveAs(Server.MapPath("~/Media/Galery" + Path.GetFileName(_HttpPostedFile.FileName)));
+            }
+        }
+    }
+}
