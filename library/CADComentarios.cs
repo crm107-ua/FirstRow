5953e7e65f7d6ff8d6636ed50eb1f7b2f973b3a9
dcr28 <dcr28@alu.ua.es>
2022-05-13 11:47:17 +0200

    49762328B Implementacion Clase Comentarios (Añadir, Eliminar, Mostrar)

--- library/CADComentarios.cs
+++ library/CADComentarios.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Configuration;
 using System.Data.SqlClient;
 
@@ -7,6 +8,7 @@ namespace library
     public class CADComentarios
     {
         private String constring;
+        ArrayList lista = new ArrayList();
 
         public CADComentarios()
         {
@@ -37,6 +39,57 @@ namespace library
             get { return _Nickname; }
             set { _Nickname = value; }
         }
+        public void InsertComennt(ENComentarios c)
+        {
+            SqlConnection nueva_conexion = new SqlConnection(constring);
 
+            try
+            {
+                nueva_conexion.Open();
+                string insert = "";
+                insert = "Insert Into CommentsContent(Id,Texto,Nickname,Estrellas) VALUES (" + c.Id + "," + c.Texto + ",'" + c.Nickname + "," + c.Estrellas + "')";
+                SqlCommand com = new SqlCommand(insert, nueva_conexion);
+
+                com.ExecuteNonQuery();
+            }
+            catch (Exception ex) { }
+            finally { nueva_conexion.Close(); }
+        }
+        public void DeleteComennt(ENComentarios c)
+        {
+            SqlConnection nueva_conexion = new SqlConnection(constring);
+
+            try
+            {
+                nueva_conexion.Open();
+                string delete = "";
+                delete = "Delete from Files where CommentsContent.Id = " + c.Id + "AND CommentsContent.Texto" + c.Texto + "AND CommentsContent.Nickname" + c.Nickname + "AND CommentsContent.Estrellas" + c.Estrellas;
+                SqlCommand com = new SqlCommand(delete, nueva_conexion);
+
+
+                com.ExecuteNonQuery();
+            }
+            catch (Exception ex) { }
+            finally { nueva_conexion.Close(); }
+        }
+        public ArrayList ShowComennts(ENComentarios comentario)
+        {
+            SqlConnection c = new SqlConnection(constring);
+            c.Open();
+            SqlCommand com = new SqlCommand("Select Content from CommentsContent WHERE Id=" + comentario.Id + " AND Texto=" + comentario.Texto + " AND Nickname=" + comentario.Nickname + " AND Estrellas=" + comentario.Estrellas, c);
+            SqlDataReader dr = com.ExecuteReader();
+
+            while (dr.Read())
+            {
+                lista.Add(dr["Content"].ToString());
+            }
+            dr.Close();
+            c.Close();
+
+            return lista;
+        }
     }
 }
+
+    
+
