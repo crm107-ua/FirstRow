1681934427444c1f8b7488883eaa8cb33c1b3d61
jcm179-ua <jcm179@alu.ua.es>
2022-05-17 13:03:47 +0200

    47431594M Correccion de errores agregacion de borrado de archivos, carga de experiencia

--- FirstRow/Pages/Seccion_Galeria.aspx.designer.cs
+++ FirstRow/Pages/Seccion_Galeria.aspx.designer.cs
@@ -78,6 +78,15 @@ namespace FirstRow.Pages
         protected global::System.Web.UI.WebControls.Panel addImg;
 
         /// <summary>
+        /// Control mostrar_experiencias.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel mostrar_experiencias;
+
+        /// <summary>
         /// Control masGaleri.
         /// </summary>
         /// <remarks>
