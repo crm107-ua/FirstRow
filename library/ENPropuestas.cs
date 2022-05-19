2679b46cbf5ef6421911b613b2ceff6a511a2701
Jose Maria Cortona López <jmcl9@mscloud.ua.es>
2022-05-19 22:05:29 +0200

    54207285B propuestas EN y CAD

--- library/ENPropuestas.cs
+++ library/ENPropuestas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,25 +10,62 @@ namespace library
     public class ENPropuestas
     {
         private int _Id;
-        private string _Imagen; 
+        private string _Descripcion;
+        private string _Imagen;
+        internal object id;
+        internal object descripcion;
 
         public int Id //GET-SET
         {
             get { return _Id; }
             set { _Id = value; }
         }
+
+        public string Descripcion
+        {
+            get { return _Descripcion; }
+            set { _Descripcion = value; }
+        }
+
         public string Imagen //GET-SET
         {
             get { return _Imagen; }
             set { _Imagen = value; }
         }
 
-        public static string _Nombre; //CRUD
+        public ENPropuestas()
+        {
+            this.Id = 0;
+            this.Descripcion = "";
+            this.Imagen = "";
 
-        public static string _Descripcion; //CRUD
+        }
 
-        public static string _Categoria; //CRUD
+        public ENPropuestas(int Id, string descripcion, string Imagen)
+        {
+            this.Id = Id;
+            this.Descripcion = descripcion;
+            this.Imagen = Imagen;
 
-        public static string _Empresa; //CRUD
+        }
+
+        public DataSet readPropuestas()
+        {
+            CADPropuestas propuesta = new CADPropuestas();
+            return propuesta.readPropuestas();
+        }
+
+        public bool deletePropuesta()
+        {
+            bool eliminado = false;
+            CADPropuestas propuesta = new CADPropuestas();
+
+            if (!propuesta.readPropuestas(this, true))
+            {
+                eliminado = propuesta.deletePropuesta(this);
+            }
+
+            return eliminado;
+        }
     }
 }
