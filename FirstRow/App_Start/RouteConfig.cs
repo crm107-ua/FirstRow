9684d0bf6d76ca5e6f8d10441973defbce0c873c
Akira Llorens Montero <alm191@alu.ua.es>
2022-05-24 21:21:46 +0200

    54488456F Cambiado nombre de StoryUsuario.aspx a Story.aspx

--- FirstRow/App_Start/RouteConfig.cs
+++ FirstRow/App_Start/RouteConfig.cs
@@ -24,9 +24,9 @@ namespace FirstRow.App_Start
             routes.MapPageRoute("sorteo", "sorteo/{slug}", "~/Pages/Sorteo.aspx");
              
             routes.MapPageRoute("stories", "stories", "~/Pages/Stories.aspx");
-            routes.MapPageRoute("story", "story/{slug}", "~/Pages/StoryUsuario.aspx");
+            routes.MapPageRoute("story", "story/{slug}", "~/Pages/Story.aspx");
             routes.MapPageRoute("agregar_story", "agregar-story", "~/Pages/Forms/FormStory.aspx");
-            routes.MapPageRoute("user-stories", "user-stories/{nickname}", "~/Pages/StoryUsuario.aspx");
+            routes.MapPageRoute("user-stories", "user-stories/{nickname}", "~/Pages/Story.aspx");
 
             routes.MapPageRoute("firstrow", "firstrow", "~/Pages/FirstRow.aspx");
 
