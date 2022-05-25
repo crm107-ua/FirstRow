240da7d75ca9dbc8c68dd3f76f05962e3124a549
Jordi Cardona <jcm179@alu.ua.es>
2022-05-25 14:28:03 +0200

    47431594M EN y Cad de admin acabados falta probar.

--- library/CADAdmin.cs
+++ library/CADAdmin.cs
@@ -99,14 +99,132 @@ namespace library
             return conseguido;
         }
 
-        public bool readAll(ENGaleria en) 
+        public bool readAll(ENAdmin en) 
         {
             bool conseguido = false;
-            
+            SqlConnection connection = new SqlConnection(constring);
+
+            try
+            {
+                connection.Open();
+                string querty = "select * form [Admin]";
+                SqlCommand command = new SqlCommand(querty, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read()) 
+                {
+                    if (reader["slug"].ToString().Split('-')[0] == "titulo")
+                    {
+                        escrituraDatos(en, reader,true);
+                    }
+                    else if (reader["slug"].ToString().Split('-')[0] == "des")
+                    {
+                        escrituraDatos(en, reader, false);
+                    }
+                    else 
+                    {
+                        if (reader["slug"].ToString().Split('-')[2] == "1")
+                        {
+                            en.ContactoTexto1 = reader["text"].ToString();
+                        }
+                        else 
+                        {
+                            en.ContactoTexto2 = reader["text"].ToString();
+                        }
+                    }
+                }
+                conseguido = true;
+
+            }
+            catch(Exception e)
+            {
+            }
+            finally 
+            {
+                conseguido.ToString();
+            }
            
 
             return conseguido;
         }
 
+        public string read(string slug) 
+        {
+
+             string text = "";
+
+            SqlConnection connection = new SqlConnection(constring);
+
+            try
+            {
+                connection.Open();
+                string querty = "select * from [Admin] where slug=@slug";
+                SqlCommand command = new SqlCommand(querty, connection);
+                command.Parameters.AddWithValue("@slug", slug);
+                SqlDataReader reader = command.ExecuteReader();
+                reader.Read();
+                text = reader["text"].ToString();
+
+            }
+            catch (Exception e)
+            {
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return text;
+        }
+
+        private static void escrituraDatos(ENAdmin en, SqlDataReader reader, bool title_desc)
+        {
+            if (title_desc)
+            {
+                switch (reader["slug"].ToString().Split('-')[1])
+                {
+                    case "exp":
+                        en.TituloExperiencia = reader["text"].ToString();
+                        break;
+
+                    case "gal":
+                        en.TituloGaleria = reader["text"].ToString();
+                        break;
+
+                    case "stories":
+                        en.TituloStories = reader["text"].ToString();
+                        break;
+                    case "blogs":
+                        en.TituloBlog = reader["text"].ToString();
+                        break;
+                    case "sorteos":
+                        en.TituloSorteos = reader["text"].ToString();
+                        break;
+                }
+            }
+            else 
+            {
+                switch (reader["slug"].ToString().Split('-')[1])
+                {
+                    case "exp":
+                        en.DescpExpperiencia = reader["text"].ToString();
+                        break;
+
+                    case "gal":
+                        en.DescpGaleria = reader["text"].ToString();
+                        break;
+
+                    case "stories":
+                        en.DescpStories = reader["text"].ToString();
+                        break;
+                    case "blogs":
+                        en.DecpBlog = reader["text"].ToString();
+                        break;
+                    case "sorteos":
+                        en.DescpSorteos = reader["text"].ToString();
+                        break;
+                }
+            }
+        }
     }
 }
