70805da1eca5d68c4a9f21344f9601b4656a286d
Jose Maria Cortona López <jmcl9@mscloud.ua.es>
2022-05-25 13:51:58 +0200

    54207285B Formulario hecho admin

--- FirstRow/Pages/Forms/admin.aspx
+++ FirstRow/Pages/Forms/admin.aspx
@@ -5,16 +5,70 @@
             <div class="top">
                 <h3 class="title">Modificacion de campos</h3>
             </div>
+          
             <div class="center">
-                <asp:TextBox ID="create_admin_title" runat="server" type="text" style=" width:100%" class="input" placeholder="Titulo"></asp:TextBox>
-                <asp:RequiredFieldValidator ID="titulo_admin_requerido" ControlToValidate="create_admin_title" validationgroup="GrupoCrearadmin" ForeColor="Red" style="float:left; margin-left: 10px; margin-top: 5px; margin-bottom: 10px;" runat="server" ErrorMessage="*Titulo requerido"></asp:RequiredFieldValidator>
+                <h5 class="subtitle">Titulo de la experiencia: </h5>
+                <asp:TextBox ID="create_admin_title" runat="server" type="text" style=" width:100%" class="input" placeholder="Titulo de la experiencia"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="titulo_exp_admin_requerido" ControlToValidate="create_admin_title" validationgroup="GrupoCrearadmin" ForeColor="Red" style="float:left; margin-left: 10px; margin-top: 5px; margin-bottom: 10px;" runat="server" ErrorMessage="*Titulo requerido"></asp:RequiredFieldValidator>
                 <asp:RegularExpressionValidator ID="titulo_admin_formato" ControlToValidate="create_admin_title" validationgroup="GrupoCrearadmin" style="float:left; margin-left: 10px; margin-top: 5px; margin-bottom: 10px;" runat="server" Display="Dynamic" ForeColor="Red" ValidationExpression="^[-.:,0-9A-Za-zÀ-ÿ ]+$" ErrorMessage="*Formato de titulo incorrecto"> </asp:RegularExpressionValidator>
 
-                <asp:TextBox ID="create_admin_descripcion" runat="server" type="text" textMode="MultiLine" style="resize:vertical;" Width="100%" Height="180px" class="textarea" placeholder="Descripcion"></asp:TextBox>
-                <asp:RequiredFieldValidator ID="descripcion_admin_requerido" ControlToValidate="create_admin_descripcion" validationgroup="GrupoCrearadmin" ForeColor="Red" style="float:left; margin-left: 10px; margin-top: 10px; margin-bottom: 5px;" runat="server" ErrorMessage="*Descripcion requerida"></asp:RequiredFieldValidator>
+                 <h5 class="subtitle">Titulo de la galeria: </h5>
+                <asp:TextBox ID="TextBox1" runat="server" type="text" style=" width:100%" class="input" placeholder="Titulo de la galeria"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="titulo_galeria_admin_requerido" ControlToValidate="TextBox1" validationgroup="GrupoCrearadmin" ForeColor="Red" style="float:left; margin-left: 10px; margin-top: 5px; margin-bottom: 10px;" runat="server" ErrorMessage="*Titulo requerido"></asp:RequiredFieldValidator>
+                <asp:RegularExpressionValidator ID="titulo_galeria_admin_formato" ControlToValidate="TextBox1" validationgroup="GrupoCrearadmin" style="float:left; margin-left: 10px; margin-top: 5px; margin-bottom: 10px;" runat="server" Display="Dynamic" ForeColor="Red" ValidationExpression="^[-.:,0-9A-Za-zÀ-ÿ ]+$" ErrorMessage="*Formato de titulo incorrecto"> </asp:RegularExpressionValidator>
+
+                 <h5 class="subtitle">Titulo de la storie: </h5>
+                <asp:TextBox ID="TextBox2" runat="server" type="text" style=" width:100%" class="input" placeholder="Titulo de la storie"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="titulo_storie_admin_requerido" ControlToValidate="TextBox2" validationgroup="GrupoCrearadmin" ForeColor="Red" style="float:left; margin-left: 10px; margin-top: 5px; margin-bottom: 10px;" runat="server" ErrorMessage="*Titulo requerido"></asp:RequiredFieldValidator>
+                <asp:RegularExpressionValidator ID="titulo_storie_admin_formato" ControlToValidate="TextBox2" validationgroup="GrupoCrearadmin" style="float:left; margin-left: 10px; margin-top: 5px; margin-bottom: 10px;" runat="server" Display="Dynamic" ForeColor="Red" ValidationExpression="^[-.:,0-9A-Za-zÀ-ÿ ]+$" ErrorMessage="*Formato de titulo incorrecto"> </asp:RegularExpressionValidator>
+
+                 <h5 class="subtitle">Titulol blog: </h5>
+                <asp:TextBox ID="TextBox3" runat="server" type="text" style=" width:100%" class="input" placeholder="Titulo del blog"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="titulo_blog_admin_requerido" ControlToValidate="TextBox3" validationgroup="GrupoCrearadmin" ForeColor="Red" style="float:left; margin-left: 10px; margin-top: 5px; margin-bottom: 10px;" runat="server" ErrorMessage="*Titulo requerido"></asp:RequiredFieldValidator>
+                <asp:RegularExpressionValidator ID="titulo_blog_admin_formato" ControlToValidate="TextBox3" validationgroup="GrupoCrearadmin" style="float:left; margin-left: 10px; margin-top: 5px; margin-bottom: 10px;" runat="server" Display="Dynamic" ForeColor="Red" ValidationExpression="^[-.:,0-9A-Za-zÀ-ÿ ]+$" ErrorMessage="*Formato de titulo incorrecto"> </asp:RegularExpressionValidator>
+
+                 <h5 class="subtitle">Titulo del sorteo: </h5>
+                <asp:TextBox ID="TextBox4" runat="server" type="text" style=" width:100%" class="input" placeholder="Titulo sorteos"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="titulo_sorteos_admin_requerido" ControlToValidate="TextBox4" validationgroup="GrupoCrearadmin" ForeColor="Red" style="float:left; margin-left: 10px; margin-top: 5px; margin-bottom: 10px;" runat="server" ErrorMessage="*Titulo requerido"></asp:RequiredFieldValidator>
+                <asp:RegularExpressionValidator ID="titulo_sorteos_admin_formato" ControlToValidate="TextBox4" validationgroup="GrupoCrearadmin" style="float:left; margin-left: 10px; margin-top: 5px; margin-bottom: 10px;" runat="server" Display="Dynamic" ForeColor="Red" ValidationExpression="^[-.:,0-9A-Za-zÀ-ÿ ]+$" ErrorMessage="*Formato de titulo incorrecto"> </asp:RegularExpressionValidator>
+
+
+
+                 <h5 class="subtitle">Descripcion experiencia: </h5>
+                <asp:TextBox ID="create_admin_descripcion" runat="server" type="text" textMode="MultiLine" style="resize:vertical;" Width="100%" Height="180px" class="textarea" placeholder="Descripcion experiencia"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="descripcion_exp_admin_requerido" ControlToValidate="create_admin_descripcion" validationgroup="GrupoCrearadmin" ForeColor="Red" style="float:left; margin-left: 10px; margin-top: 10px; margin-bottom: 5px;" runat="server" ErrorMessage="*Descripcion requerida"></asp:RequiredFieldValidator>
                 <asp:RegularExpressionValidator ID="descripcion_admin_formato" ControlToValidate="create_admin_descripcion" validationgroup="GrupoCrearadmin" style="float:left; margin-left: 10px; margin-top: 10px; margin-bottom: 5px;" runat="server" Display="Dynamic" ForeColor="Red" ValidationExpression="^[-.:,0-9A-Za-zÀ-ÿ\s]+$" ErrorMessage="*Formato de descripcion incorrecto"> </asp:RegularExpressionValidator>
 
-                </div> 
+                  <h5 class="subtitle">Descripcion galeria: </h5>
+                <asp:TextBox ID="TextBox5" runat="server" type="text" textMode="MultiLine" style="resize:vertical;" Width="100%" Height="180px" class="textarea" placeholder="Descripcion galeria"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="descripcion_galeria_admin_requerido" ControlToValidate="TextBox5" validationgroup="GrupoCrearadmin" ForeColor="Red" style="float:left; margin-left: 10px; margin-top: 10px; margin-bottom: 5px;" runat="server" ErrorMessage="*Descripcion requerida"></asp:RequiredFieldValidator>
+                <asp:RegularExpressionValidator ID="descripcion_galeria_admin_formato" ControlToValidate="TextBox5" validationgroup="GrupoCrearadmin" style="float:left; margin-left: 10px; margin-top: 10px; margin-bottom: 5px;" runat="server" Display="Dynamic" ForeColor="Red" ValidationExpression="^[-.:,0-9A-Za-zÀ-ÿ\s]+$" ErrorMessage="*Formato de descripcion incorrecto"> </asp:RegularExpressionValidator>
+
+                  <h5 class="subtitle">Descripcion stories: </h5>
+                <asp:TextBox ID="TextBox6" runat="server" type="text" textMode="MultiLine" style="resize:vertical;" Width="100%" Height="180px" class="textarea" placeholder="Descripcion stories"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="descripcion_stories_admin_requerido" ControlToValidate="TextBox6" validationgroup="GrupoCrearadmin" ForeColor="Red" style="float:left; margin-left: 10px; margin-top: 10px; margin-bottom: 5px;" runat="server" ErrorMessage="*Descripcion requerida"></asp:RequiredFieldValidator>
+                <asp:RegularExpressionValidator ID="descripcion_stories_admin_formato" ControlToValidate="TextBox6" validationgroup="GrupoCrearadmin" style="float:left; margin-left: 10px; margin-top: 10px; margin-bottom: 5px;" runat="server" Display="Dynamic" ForeColor="Red" ValidationExpression="^[-.:,0-9A-Za-zÀ-ÿ\s]+$" ErrorMessage="*Formato de descripcion incorrecto"> </asp:RegularExpressionValidator>
+
+                  <h5 class="subtitle">Descripcion blogs: </h5>
+                <asp:TextBox ID="TextBox7" runat="server" type="text" textMode="MultiLine" style="resize:vertical;" Width="100%" Height="180px" class="textarea" placeholder="Descripcion blogs"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="descripcion_blogs_admin_requerido" ControlToValidate="TextBox7" validationgroup="GrupoCrearadmin" ForeColor="Red" style="float:left; margin-left: 10px; margin-top: 10px; margin-bottom: 5px;" runat="server" ErrorMessage="*Descripcion requerida"></asp:RequiredFieldValidator>
+                <asp:RegularExpressionValidator ID="descripcion_blogs_admin_formato" ControlToValidate="TextBox7" validationgroup="GrupoCrearadmin" style="float:left; margin-left: 10px; margin-top: 10px; margin-bottom: 5px;" runat="server" Display="Dynamic" ForeColor="Red" ValidationExpression="^[-.:,0-9A-Za-zÀ-ÿ\s]+$" ErrorMessage="*Formato de descripcion incorrecto"> </asp:RegularExpressionValidator>
+
+                  <h5 class="subtitle">Descripcion sorteos: </h5>
+                <asp:TextBox ID="TextBox8" runat="server" type="text" textMode="MultiLine" style="resize:vertical;" Width="100%" Height="180px" class="textarea" placeholder="Descripcion sorteos"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="descripcion_sorteos_admin_requerido" ControlToValidate="TextBox8" validationgroup="GrupoCrearadmin" ForeColor="Red" style="float:left; margin-left: 10px; margin-top: 10px; margin-bottom: 5px;" runat="server" ErrorMessage="*Descripcion sorteos"></asp:RequiredFieldValidator>
+                <asp:RegularExpressionValidator ID="descripcion_sorteos_admin_formato" ControlToValidate="TextBox8" validationgroup="GrupoCrearadmin" style="float:left; margin-left: 10px; margin-top: 10px; margin-bottom: 5px;" runat="server" Display="Dynamic" ForeColor="Red" ValidationExpression="^[-.:,0-9A-Za-zÀ-ÿ\s]+$" ErrorMessage="*Formato de descripcion incorrecto"> </asp:RegularExpressionValidator>
+                   
+                  <h5 class="subtitle">Slogan: </h5>
+                                <asp:TextBox ID="TextBox9" runat="server" type="text" textMode="MultiLine" style="resize:vertical;" Width="100%" Height="180px" class="textarea" placeholder="Slogan texto"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="slogan_requerido" ControlToValidate="TextBox9" validationgroup="GrupoCrearadmin" ForeColor="Red" style="float:left; margin-left: 10px; margin-top: 10px; margin-bottom: 5px;" runat="server" ErrorMessage="*Descripcion requerida"></asp:RequiredFieldValidator>
+                <asp:RegularExpressionValidator ID="slogan_formato" ControlToValidate="TextBox9" validationgroup="GrupoCrearadmin" style="float:left; margin-left: 10px; margin-top: 10px; margin-bottom: 5px;" runat="server" Display="Dynamic" ForeColor="Red" ValidationExpression="^[-.:,0-9A-Za-zÀ-ÿ\s]+$" ErrorMessage="*Formato de descripcion incorrecto"> </asp:RegularExpressionValidator>
+
+                  <h5 class="subtitle">Info: </h5>
+                                <asp:TextBox ID="TextBox10" runat="server" type="text" textMode="MultiLine" style="resize:vertical;" Width="100%" Height="180px" class="textarea" placeholder="Info texto"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="info_text" ControlToValidate="TextBox10" validationgroup="GrupoCrearadmin" ForeColor="Red" style="float:left; margin-left: 10px; margin-top: 10px; margin-bottom: 5px;" runat="server" ErrorMessage="*Descripcion requerida"></asp:RequiredFieldValidator>
+                <asp:RegularExpressionValidator ID="info_formato" ControlToValidate="TextBox10" validationgroup="GrupoCrearadmin" style="float:left; margin-left: 10px; margin-top: 10px; margin-bottom: 5px;" runat="server" Display="Dynamic" ForeColor="Red" ValidationExpression="^[-.:,0-9A-Za-zÀ-ÿ\s]+$" ErrorMessage="*Formato de descripcion incorrecto"> </asp:RegularExpressionValidator>
+               
             </div>
             <div class="bottom">
                 <asp:Button ID="btnCrea" class="submit button" runat="server" Text="Modificar" OnClick="btnCrea_Click" ValidationGroup="GrupoCrearadmin"/>
--- FirstRow/Pages/Forms/admin.aspx.designer.cs
+++ FirstRow/Pages/Forms/admin.aspx.designer.cs
@@ -24,13 +24,13 @@ namespace FirstRow.Pages.Forms
         protected global::System.Web.UI.WebControls.TextBox create_admin_title;
 
         /// <summary>
-        /// Control titulo_admin_requerido.
+        /// Control titulo_exp_admin_requerido.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.RequiredFieldValidator titulo_admin_requerido;
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator titulo_exp_admin_requerido;
 
         /// <summary>
         /// Control titulo_admin_formato.
@@ -42,6 +42,114 @@ namespace FirstRow.Pages.Forms
         protected global::System.Web.UI.WebControls.RegularExpressionValidator titulo_admin_formato;
 
         /// <summary>
+        /// Control TextBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+
+        /// <summary>
+        /// Control titulo_galeria_admin_requerido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator titulo_galeria_admin_requerido;
+
+        /// <summary>
+        /// Control titulo_galeria_admin_formato.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator titulo_galeria_admin_formato;
+
+        /// <summary>
+        /// Control TextBox2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+
+        /// <summary>
+        /// Control titulo_storie_admin_requerido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator titulo_storie_admin_requerido;
+
+        /// <summary>
+        /// Control titulo_storie_admin_formato.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator titulo_storie_admin_formato;
+
+        /// <summary>
+        /// Control TextBox3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox3;
+
+        /// <summary>
+        /// Control titulo_blog_admin_requerido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator titulo_blog_admin_requerido;
+
+        /// <summary>
+        /// Control titulo_blog_admin_formato.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator titulo_blog_admin_formato;
+
+        /// <summary>
+        /// Control TextBox4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox4;
+
+        /// <summary>
+        /// Control titulo_sorteos_admin_requerido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator titulo_sorteos_admin_requerido;
+
+        /// <summary>
+        /// Control titulo_sorteos_admin_formato.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator titulo_sorteos_admin_formato;
+
+        /// <summary>
         /// Control create_admin_descripcion.
         /// </summary>
         /// <remarks>
@@ -51,13 +159,13 @@ namespace FirstRow.Pages.Forms
         protected global::System.Web.UI.WebControls.TextBox create_admin_descripcion;
 
         /// <summary>
-        /// Control descripcion_admin_requerido.
+        /// Control descripcion_exp_admin_requerido.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.RequiredFieldValidator descripcion_admin_requerido;
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator descripcion_exp_admin_requerido;
 
         /// <summary>
         /// Control descripcion_admin_formato.
@@ -67,6 +175,168 @@ namespace FirstRow.Pages.Forms
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.RegularExpressionValidator descripcion_admin_formato;
+
+        /// <summary>
+        /// Control TextBox5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox5;
+
+        /// <summary>
+        /// Control descripcion_galeria_admin_requerido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator descripcion_galeria_admin_requerido;
+
+        /// <summary>
+        /// Control descripcion_galeria_admin_formato.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator descripcion_galeria_admin_formato;
+
+        /// <summary>
+        /// Control TextBox6.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox6;
+
+        /// <summary>
+        /// Control descripcion_stories_admin_requerido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator descripcion_stories_admin_requerido;
+
+        /// <summary>
+        /// Control descripcion_stories_admin_formato.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator descripcion_stories_admin_formato;
+
+        /// <summary>
+        /// Control TextBox7.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox7;
+
+        /// <summary>
+        /// Control descripcion_blogs_admin_requerido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator descripcion_blogs_admin_requerido;
+
+        /// <summary>
+        /// Control descripcion_blogs_admin_formato.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator descripcion_blogs_admin_formato;
+
+        /// <summary>
+        /// Control TextBox8.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox8;
+
+        /// <summary>
+        /// Control descripcion_sorteos_admin_requerido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator descripcion_sorteos_admin_requerido;
+
+        /// <summary>
+        /// Control descripcion_sorteos_admin_formato.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator descripcion_sorteos_admin_formato;
+
+        /// <summary>
+        /// Control TextBox9.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox9;
+
+        /// <summary>
+        /// Control slogan_requerido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator slogan_requerido;
+
+        /// <summary>
+        /// Control slogan_formato.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator slogan_formato;
+
+        /// <summary>
+        /// Control TextBox10.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox10;
+
+        /// <summary>
+        /// Control info_text.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator info_text;
+
+        /// <summary>
+        /// Control info_formato.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator info_formato;
 
         /// <summary>
         /// Control btnCrea.
