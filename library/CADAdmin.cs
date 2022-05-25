4baab8bfe40a0f70f5e825203348776a5cf5196c
Jordi Cardona <jcm179@alu.ua.es>
2022-05-25 13:52:35 +0200

    47431594M EN y CAD de admin para el editado

--- library/CADAdmin.cs
+++ library/CADAdmin.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace library
 {
@@ -16,28 +17,95 @@ namespace library
             constring = ConfigurationManager.ConnectionStrings["DataBase"].ToString();
         }
 
-        public bool obtenerPlantilla(ENAdmin en)
+        public bool modify(ENAdmin en) 
         {
-            bool creado = false;
-            return creado;
+            bool conseguido = false;
+
+            SqlConnection connection = new SqlConnection(constring);
+
+            try
+            {
+                connection.Open();
+                string querty = "update [Admin] set text=@text where slug='titulo-exp'";
+                SqlCommand command = new SqlCommand(querty, connection);
+                command.Parameters.AddWithValue("@text", en.TituloExperiencia);
+                command.ExecuteNonQuery();
+
+                querty = "update [Admin] set text=@text where slug='des-exp'";
+                command = new SqlCommand(querty, connection);
+                command.Parameters.AddWithValue("@text", en.DescpExpperiencia);
+                command.ExecuteNonQuery();
+
+                querty = "update [Admin] set text=@text where slug='titulo-gal'";
+                command = new SqlCommand(querty, connection);
+                command.Parameters.AddWithValue("@text", en.TituloGaleria);
+                command.ExecuteNonQuery();
+
+                querty = "update [Admin] set text=@text where slug='des-gal'";
+                command = new SqlCommand(querty, connection);
+                command.Parameters.AddWithValue("@text", en.DescpGaleria);
+                command.ExecuteNonQuery();
+
+                querty = "update [Admin] set text=@text where slug='titulo-stories'";
+                command = new SqlCommand(querty, connection);
+                command.Parameters.AddWithValue("@text", en.TituloStories);
+                command.ExecuteNonQuery();
+
+                querty = "update [Admin] set text=@text where slug='des-stories'";
+                command = new SqlCommand(querty, connection);
+                command.Parameters.AddWithValue("@text", en.DescpStories);
+                command.ExecuteNonQuery();
+
+                querty = "update [Admin] set text=@text where slug='titulo-blogs'";
+                command = new SqlCommand(querty, connection);
+                command.Parameters.AddWithValue("@text", en.TituloBlog);
+                command.ExecuteNonQuery();
+
+                querty = "update [Admin] set text=@text where slug='des-exp'";
+                command = new SqlCommand(querty, connection);
+                command.Parameters.AddWithValue("@text", en.DescpExpperiencia);
+                command.ExecuteNonQuery();
+
+                querty = "update [Admin] set text=@text where slug='titulo-sorteos'";
+                command = new SqlCommand(querty, connection);
+                command.Parameters.AddWithValue("@text", en.TituloSorteos);
+                command.ExecuteNonQuery();
+
+                querty = "update [Admin] set text=@text where slug='des-sorteos'";
+                command = new SqlCommand(querty, connection);
+                command.Parameters.AddWithValue("@text", en.DescpSorteos);
+                command.ExecuteNonQuery();
+
+                querty = "update [Admin] set text=@text where slug='contacto-texto-1'";
+                command = new SqlCommand(querty, connection);
+                command.Parameters.AddWithValue("@text", en.ContactoTexto1);
+                command.ExecuteNonQuery();
+
+                querty = "update [Admin] set text=@text where slug='contacto-texto-2'";
+                command = new SqlCommand(querty, connection);
+                command.Parameters.AddWithValue("@text", en.ContactoTexto2);
+                command.ExecuteNonQuery();
+
+                conseguido = true;
+            }
+            catch (Exception e)
+            {
+            }
+            finally 
+            {
+                connection.Close();
+            }
+
+            return conseguido;
         }
 
-        public bool crearPlantilla(ENAdmin en)
+        public bool readAll(ENGaleria en) 
         {
-            bool creado = false;
-            return creado;
-        }
+            bool conseguido = false;
+            
+           
 
-        public bool modificarPlantilla(ENAdmin en)
-        {
-            bool modificado = false;
-            return modificado;
-        }
-
-        public bool eliminarPlantilla(ENAdmin en)
-        {
-            bool eliminado = false;
-            return eliminado;
+            return conseguido;
         }
 
     }
