53cc9fade7e29139634f7c024e65296a7999cba2
jcm179-ua <jcm179@alu.ua.es>
2022-05-25 08:54:24 +0200

    47431594M Areglos en cadSorteos

--- library/CADSorteos.cs
+++ library/CADSorteos.cs
@@ -17,9 +17,8 @@ namespace library
         /*
          insert into sorteos values ('sorteo de experiencia por el atlantico','lo que su nombre indica','sorteo de experiencia por el atlantico','fuente.jpg',40,'2021-05-08','2021-05-10','Jet Enterprise')
          */
-        internal bool addParticipante(ENSorteos en)
+        internal bool addSorteo(ENSorteos en)
         {
-            //TODO: addParticipante
             bool anadido = false;
             SqlConnection c = new SqlConnection(constring);
             try
@@ -27,9 +26,16 @@ namespace library
 
                 c.Open();
 
-                string s = "Insert INTO [dbo].[Sorteo_Usuarios] (titulo,descripcion,slug,imagen,FechaInicio,FechaFinal) VALUES ('" + en.Titulo + "','" + en.Descripcion + "','" + en.Slug + "','" + en.Imagen + "','" + en.FechaInicio + "','" + en.FechaFinal + "')";
+                string s = "Insert INTO [dbo].[Sorteo_Usuarios] (titulo,descripcion,slug,imagen,FechaInicio,FechaFinal) VALUES ( @titulo , @descripcion , @slug , @imagen, @fechainicio, @fechafinal)";
 
                 SqlCommand com = new SqlCommand(s, c);
+                com.Parameters.AddWithValue("@titulo", en.Titulo);
+                com.Parameters.AddWithValue("@descripcion", en.Descripcion);
+                com.Parameters.AddWithValue("@slug", en.Slug);
+                com.Parameters.AddWithValue("@imagen",en.Imagen);
+                com.Parameters.AddWithValue("@fechainicio", en.FechaInicio.ToString("yyyy-MM-dd HH:mm:ss.ffffff"));
+                com.Parameters.AddWithValue("@fechafinal", en.FechaFinal.ToString("yyyy-MM-dd HH:mm:ss.ffffff"));
+
                 com.ExecuteNonQuery();
                 anadido = true;
             }
@@ -50,6 +56,32 @@ namespace library
             }
             return anadido;
 
+        }
+
+        internal bool addParticipante(ENSorteos eNSorteos) 
+        {
+            bool conseguido = false;
+            SqlConnection conection = new SqlConnection(constring);
+            foreach (ENUsuario usuario in eNSorteos.Participantes) {
+                try
+                {
+                    conection.Open();
+                    string querty = "insert into [Sorteo_Usuarios] (id_Sorteo, nickname_Usuario) values (@sorteo,@usuario)";
+                    SqlCommand com = new SqlCommand(querty, conection);
+                    com.Parameters.AddWithValue("@sorteo", eNSorteos.Id);
+                    com.Parameters.AddWithValue("@usuario",usuario.nickname);
+    
+                }
+                catch (Exception e)
+                {
+                }
+                finally
+                {
+                    conection.Close();
+                }
+            }
+
+            return conseguido;
         }
 
         internal bool deleteParticipante(ENSorteos eNSorteos)
@@ -125,7 +157,8 @@ namespace library
         
 
 
-    }
+        }
+
         public bool readsorteo( ENSorteos sorteo)
         {
 
