429f0c797ca0e134aa576a0bd875ea8b11344d93
jcbs6 <jcbs6@alu.ua.es>
2022-05-27 00:49:21 +0200

    Y5164813Q Actualizacion FormPropuestas.aspx

--- FirstRow/Pages/Forms/FormPropuesta.aspx.cs
+++ FirstRow/Pages/Forms/FormPropuesta.aspx.cs
@@ -20,9 +20,36 @@ namespace FirstRow.Pages.Forms
 
         protected void CrearPropuesta(object sender, EventArgs e)
         {
+            Random rand = new Random();
+            ENPropuestas propuesta = new ENPropuestas();
+            ENUsuario usuario = (ENUsuario)Session["usuario"];
+
+            propuesta.Titulo = create_propuesta_title.Text.Trim();
+            propuesta.Descripcion = create_propuesta_descripcion.Text.Trim();
+            propuesta.Slug = Home.slug(propuesta.Titulo) + "-" + rand.Next(1, 999999).ToString();
+            propuesta.Usuario.nickname = usuario.nickname;
+
+            foreach (HttpPostedFile imagenes in crear_propuesta_imagenes.PostedFiles)
+            {
+                if (crear_propuesta_imagenes.HasFiles)
+                {
+                    string imagen = propuesta.Slug + "-propuesta-" + Path.GetFileName(imagenes.FileName);
+                    propuesta.Imagenes.Add(new ENImagenes(imagen));
+                    imagenes.SaveAs(Server.MapPath("~/Media/Propuestas/") + imagen);
+                }
+            }
+            if (propuesta.newPropuesta())
+            {
+                Response.Redirect("/propuesta/" + "/" + propuesta.Slug);
+            }
+            else
+            {
+                resultado.Text = "Ha ocurrido un error";
+            }
 
         }
+
     }
-}
+    }
   
         
