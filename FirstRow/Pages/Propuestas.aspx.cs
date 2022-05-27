df7c4dd63cd45e9f36da2c5c0a32fb275ba43bc1
jcbs6 <jcbs6@alu.ua.es>
2022-05-27 12:47:14 +0200

    Y5164813Q Actualizacion Propuestas.aspx.cs

--- FirstRow/Pages/Propuestas.aspx.cs
+++ FirstRow/Pages/Propuestas.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using library;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,19 @@ namespace FirstRow.Pages
         protected void Page_Load(object sender, EventArgs e)
         {
             if (Session["usuario"] != null) { add_form.Visible = true; } else { add_form.Visible = false; }
+
+            Application["propuestas_title"] = ENAdmin.read("titulo-propuestas");
+            Application["propuestas_subtitle"] = ENAdmin.read("des-propuestas");
+            propuestas_title2.Text = (string)Application["propuestas_title"];
+            propuestas_subtitle.InnerText = (string)Application["propuestas_subtitle"];
+            /*
+             "url(https://static.onecms.io/wp-content/uploads/sites/28/2021/09/24/travel-gifts-lead-TRVLGG0921.jpg)"
+             */
+            background_image_header.Style.Add("background-image", "url(https://static.onecms.io/wp-content/uploads/sites/28/2021/09/24/travel-gifts-lead-TRVLGG0921.jpg)");
+
+            ENPropuestas propuesta = new ENPropuestas();
+            List<ENPropuestas> lista = new List<ENPropuestas>();
+            propuesta.readpropuestasconectado(lista);
         }
-    }
+        }
 }
